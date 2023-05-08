e2b42ba77d2d87738a03973f5cf6ca5ae68b6b79
ShadowGhosto <115074349+ShadowGhosto@users.noreply.github.com>
2023-05-08 11:42:15 +0300

    Work#4

--- №4 (вар№12).cpp
+++ №4 (вар№12).cpp
@@ -99,7 +99,7 @@ public:
 	virtual void print() {
 		int i = 0;
 		while (p[i] != '\0') {
-			cout << p[i];
+			cout << p[i]<<" ";
 			i++;
 		}
 	}
@@ -139,31 +139,26 @@ public:
 		capacity = s.capacity;
 		len = s.len;
 		p = new char[capacity];
-
 		for (int i = 0; i < len; ++i) {
 			p[i] = s.p[i];
 		}
 		p[len] = '\0';
 	}
 
-	void getDigitsArray(String s) {
-		int* arr = new int[s.Length()];
+	String getDigitsArray() {
+		String a;
 		int j = 0;
-		int arr1 = 0;
-		for (int i = 0; i < s.Length(); i++) {
-			if (s[i] >= '0' && s[i] <= '9') {
-				arr[j] = s[i] - '0';
-				j++;
-			}
 
-		
+		for (int i = 0; p[i] != '\0'; i++) {
+			if (p[i] >= '0' && p[i] <= '9') {
+				a.p[j] = p[i];
+				j++;
+
+			}
+			a.len = j;
+			a.p[j] = '\0';
 		}
-		int size = j;
-		
-		cout << "Result getDigitsArray: ";
-		for (int i = 0; i < size; i++) {
-			cout << arr[i] << " ";	
-		}
+		return a;
 	}
 };
 
@@ -171,14 +166,15 @@ public:
 int main() {
 	String a;
 	String s1 = ("Hello 123 World 456");
-	String s2 = s1;
-	String s3 = ("Hello 789 World 123");
-	String s4(s3);
+	String s2 = ("Hello 789 World 123");
 	
-	a.getDigitsArray(s1);
-	a.getDigitsArray(s2);
-	a.getDigitsArray(s3);
-	a.getDigitsArray(s4);
+	a = s1.getDigitsArray();
+	cout << "getDigitsArray: ";
+	a.print();
+	a = s2.getDigitsArray();
+	cout << "getDigitsArray: ";
+	a.print();
+
 
 	return 0;
 }
